c40cc252ae3329a14d51673141982f116540cd85
omargt8 <etejada4@gmail.com>
2019-12-24 14:12:38 -0600

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8081/proyecto201219/';
+$config['base_url'] = 'https://prueba241219.herokuapp.com/';
 //$config['base_url'] = 'http://localhost/pruebatrabajo/';
 
 /*
